be4323604657502ff1a8ff60576c43898c698b42
Mauricio Krzesinski Junior <mauriciokj@gmail.com>
2013-05-14 13:48:29 -0300

    my first commit

---
+++ SSO-RAILS/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SSORails::Application.config.secret_token = '78f8d59f417898eee060fec6781732a04ddd029df6a7d52687576fff92e71aa362c559887878effbd28b24b48118f730d8167c8e6bae5d3376cdb78bc23b034e'
